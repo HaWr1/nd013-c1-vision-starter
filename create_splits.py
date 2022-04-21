c7ff2cf12f2e0c351d6e1b68c93ccf53c18f4f64
Hannes Winkler <winklerh84@gmail.com>
2022-04-21 12:44:54 +0200

    implemented dataset splitting

--- create_splits.py
+++ create_splits.py
@@ -1,9 +1,10 @@
 import argparse
-import glob
 import os
 import random
+import math
+import shutil
 
-import numpy as np
+import pandas as pd
 
 from utils import get_module_logger
 
@@ -17,17 +18,102 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+
+    # read the statistics that we created during EDA
+    df = pd.read_pickle("data_statistics.pkl")
+
+    # generate lists with day and night samples
+    files = df.loc[:, "filename"]
+    night = df.loc[:, "night"]
+    day_files = list(files[~night])
+    night_files = list(files[night])
+
+    logger.info(f"Found {len(files)} data files.")
+
+    # shuffle the file lists
+    random.shuffle(night_files)
+    random.shuffle(day_files)
+
+    # calculate number of files for each split and for day and night
+    validation_size = 0.15
+    test_size = 0.10
+
+    n_day_val_files = math.ceil(len(day_files) * validation_size)
+    n_day_test_files = math.ceil(len(day_files) * test_size)
+    n_night_val_files = math.ceil(len(night_files) * validation_size)
+    n_night_test_files = math.ceil(len(night_files) * test_size)
+
+    train_files = []
+    validation_files = []
+    test_files = []
+
+    # pick the smaller validation and test splits first
+    for _ in range(n_day_val_files):
+        validation_files.append(day_files.pop())
+    for _ in range(n_day_test_files):
+        test_files.append(day_files.pop())
+
+    for _ in range(n_night_val_files):
+        validation_files.append(night_files.pop())
+    for _ in range(n_night_test_files):
+        test_files.append(night_files.pop())
+
+    # take remainder for training
+    for elem in day_files:
+        train_files.append(elem)
+
+    for elem in night_files:
+        train_files.append(elem)
+
+    logger.info(
+        f"Took {len(train_files)} files for training dataset ({100*len(train_files)/len(files):.2f}%)"
+    )
+    logger.info(
+        f"Took {len(validation_files)} files for validation dataset ({100*len(validation_files)/len(files):.2f}%)"
+    )
+    logger.info(
+        f"Took {len(test_files)} files for testing dataset ({100*len(test_files)/len(files):.2f}%)"
+    )
+
+    # create folders
+    folders = ["train", "val", "test"]
+    for folder in folders:
+        try:
+            os.makedirs(destination + "/" + folder)
+        except FileExistsError:
+            pass
+
+    # move the files in their target folders
+    logger.info("Moving files to target folders")
+    try:
+        for file in test_files:
+            shutil.move(source + "/" + file, destination + "/test" + file)
+        for file in validation_files:
+            shutil.move(source + "/" + file, destination + "/val")
+        for file in train_files:
+            shutil.move(source + "/" + file, destination + "/train")
+    except FileNotFoundError:
+        logger.error("File not found. Check input path")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
-                        help='source data directory')
-    parser.add_argument('--destination', required=True,
-                        help='destination data directory')
+    parser = argparse.ArgumentParser(
+        description="Split data into training / validation / testing"
+    )
+    parser.add_argument(
+        "--source",
+        required=False,
+        help="source data directory",
+        default="data/processed",
+    )
+    parser.add_argument(
+        "--destination",
+        required=False,
+        help="destination data directory",
+        default="data/splitted",
+    )
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
-    logger.info('Creating splits...')
+    logger.info("Creating splits...")
     split(args.source, args.destination)
